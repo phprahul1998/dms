00b39e9572d94c6011631678fe6504d95aa8bee2
Rahul Kumar <rahul.kumar@softage.net>
2024-08-13 17:53:03 +0530

    login

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,21 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
+
 export async function middleware(request) {
   const sessionToken = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = request.nextUrl;
 
-  if (!sessionToken && pathname !== '/login' && !request.nextUrl.pathname.startsWith('/js/')) {
+  if (!sessionToken && pathname !== '/login' && !pathname.startsWith('/js/')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
+
   if (sessionToken && pathname === '/login') {
     return NextResponse.redirect(new URL('/all-file', request.url));
   }
+
   return NextResponse.next();
 }
+
 export const config = {
   matcher: [
     '/((?!api|_next/static|_next/image|assets|forget-password|signup|public|.*\\.png$).*)',
