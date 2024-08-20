edb188e7af04aefbc9617de703a9a04c14dafd24
Rahul Kumar <rahul.kumar@softage.net>
2024-08-20 10:17:36 +0530

    console middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,17 +4,16 @@ import { NextResponse } from 'next/server';
 export async function middleware(request) {
   const sessionToken = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = request.nextUrl;
-  console.log(pathname)
-  console.log(sessionToken)
-  // if (!sessionToken && pathname !== '/login' && !pathname.startsWith('/js/')) {
-  //   return NextResponse.redirect(new URL('/login', request.url), 302);
-  // }
+  console.log('Rahul'+sessionToken)
+  if (!sessionToken && pathname !== '/login' && !pathname.startsWith('/js/')) {
+    return NextResponse.redirect(new URL('/login', request.url), 302);
+  }
 
-  // if (sessionToken && pathname === '/login') {
-  //   return NextResponse.redirect(new URL('/all-file', request.url), 302);
-  // }
+  if (sessionToken && pathname === '/login') {
+    return NextResponse.redirect(new URL('/all-file', request.url), 302);
+  }
 
-  // return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
